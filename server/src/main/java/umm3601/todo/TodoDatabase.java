f5c1155a380db1a9a3006cb785cf74ba23c41ce3
Jack Perala <peralaj9>
2020-02-23 08:42:13 -0600

    Creating the TodoDatabase
    Created the todo database including functions such as getTodo, listTodos, filterByOwner, and filterByCategory.

---
+++ server/src/main/java/umm3601/todo/TodoDatabase.java
@@ -0,0 +1,92 @@
+package umm3601.todo;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+import io.javalin.http.BadRequestResponse;
+
+/* A fake "database" of todo info*/
+public class TodoDatabase {
+
+  private Todo[] allTodos;
+
+  public TodoDatabase(String todoDataFile) throws IOException {
+    Gson gson = new Gson();
+    InputStreamReader reader = new InputStreamReader(getClass().getResourceAsStream(todoDataFile));
+    allTodos = gson.fromJson(reader, Todo[].class);
+  }
+
+  public int size() {
+    return allTodos.length;
+  }
+
+  /**
+   * Get the todo specified by the given ID. Return `null` if there is no
+   * todo with that ID.
+   *
+   * @param owner the owner of the desired todo
+   * @return the todo with the given owner, or null if there is no todo with that owner
+   */
+  public Todo getTodo(String owner) {
+    return Arrays.stream(allTodos).filter(x -> x.owner.equals(owner)).findFirst().orElse(null);
+  }
+
+  /**
+   * Get an array of all the todos satisfying the queries in the params.
+   *
+   * @param queryParams map of key-value pairs for the query
+   * @return an array of all the todos matching the given criteria
+   */
+  public Todo[] listTodos(Map<String, List<String>> queryParams) {
+    Todo[] filteredTodos = allTodos;
+
+    // Filter owner if defined
+    if (queryParams.containsKey("owner")) {
+      String ownerParam = queryParams.get("owner").get(0);
+      try {
+        String targetOwner = "owner";
+        filteredTodos = filterTodosByOwner(filteredTodos, targetOwner);
+      } catch (NumberFormatException e) {
+        throw new BadRequestResponse("Specified owner '" + ownerParam + "' can't be found");
+      }
+    }
+    // Filter cateogy if defined
+    if (queryParams.containsKey("category")) {
+      String targetCategory = queryParams.get("category").get(0);
+      filteredTodos = filterTodosByCategory(filteredTodos, targetCategory);
+    }
+    // Process other query parameters here...
+
+    return filteredTodos;
+  }
+
+  /**
+   * Get an array of all the users having the target age.
+   *
+   * @param users     the list of users to filter by age
+   * @param targetAge the target age to look for
+   * @return an array of all the users from the given list that have the target
+   *         age
+   */
+  public Todo[] filterTodosByOwner(Todo[] todos, String targetOwner) {
+    return Arrays.stream(todos).filter(x -> x.owner == targetOwner).toArray(Todo[]::new);
+  }
+
+  /**
+   * Get an array of all the users having the target company.
+   *
+   * @param users         the list of users to filter by company
+   * @param targetCompany the target company to look for
+   * @return an array of all the users from the given list that have the target
+   *         company
+   */
+  public Todo[] filterTodosByCategory(Todo[] todos, String targetCategory) {
+    return Arrays.stream(todos).filter(x -> x.category.equals(targetCategory)).toArray(Todo[]::new);
+  }
+
+}
